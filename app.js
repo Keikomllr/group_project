8e5cfd6dcf84b2cd51058620afd38b2854431e4b
Charalampos Vournazos <h.vournazos@hotmail.com>
2025-01-28 09:52:29 +0100

    work on basket and checkout

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var checkoutRouter = require('./routes/checkout');
-// Import and use the products route
+var checkoutRouter = require('./routes/checkout')
 const productsRouter = require("./routes/products");
+var basketRouter = require('./routes/basket');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: 'supersecret',
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,7 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/checkout', checkoutRouter);
-app.use('/products', productsRouter); //Import and use the products route
+app.use('/products', productsRouter);
+app.use('/basket', basketRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
